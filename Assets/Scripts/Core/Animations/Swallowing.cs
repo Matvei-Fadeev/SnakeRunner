b4736804207ed8b3e4cbbc5e57cf02071c6cd153
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-01 23:00:53 +0300

    Add: animation for food swallowing

---
+++ Assets/Scripts/Core/Animations/Swallowing.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Core.Animations {
+	public class Swallowing : MonoBehaviour {
+		[SerializeField] private string playerTag = "Player";
+		[SerializeField] private float duration = 0.2f;
+		[SerializeField] private Vector3 finalRotation;
+		
+		private void OnTriggerEnter(Collider player) {
+			if (player.CompareTag(playerTag)) {
+				StartSwallowing(player.gameObject);
+			}
+		}
+
+		private void StartSwallowing(GameObject player) {
+			transform.DOMoveX(transform.position.x, duration);
+			transform.DOScale(Vector3.zero, duration);
+			transform.DORotate(finalRotation, duration);
+		}
+	}
+}
