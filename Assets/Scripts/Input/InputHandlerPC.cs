8e8801bbe895d53748e47d2fbde44f7a546e00c5
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-01 14:52:03 +0300

    Add: input handler for PC

---
+++ Assets/Scripts/Input/InputHandlerPC.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Input {
+	public class InputHandlerPC : InputHandler {
+		public override float GetHorizontal() {
+			return UnityEngine.Input.GetAxis("Horizontal");
+		}
+
+		public override float GetVertical() {
+			return UnityEngine.Input.GetAxis("Vertical");
+		}
+
+		public override Vector2 GetDirection() {
+			Vector2 direction = new Vector2(GetHorizontal(), GetVertical());
+			return direction;
+		}
+	}
+}
