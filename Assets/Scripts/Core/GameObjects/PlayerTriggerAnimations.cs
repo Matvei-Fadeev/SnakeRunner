76542837b87c7ee651b3bf2ffe47d5845c1b07b0
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-02 13:18:45 +0300

    Add: trigger player the animations
    Start play the animations which gameobject contains when player enter to the trigger.

---
+++ Assets/Scripts/Core/GameObjects/PlayerTriggerAnimations.cs
@@ -0,0 +1,23 @@
+﻿using Core.Animations;
+using UnityEngine;
+
+namespace Core.GameObjects {
+	public class PlayerTriggerAnimations : MonoBehaviour {
+		[SerializeField] private string playerTag = "Player";
+
+		private IAnimations[] _animations;
+
+		private void Awake() {
+			_animations = GetComponents<IAnimations>();
+		}
+
+		private void OnTriggerEnter(Collider player) {
+			if (player.CompareTag(playerTag)) {
+				var playerPosition = player.transform.position;
+				foreach (var animation in _animations) {
+					animation.Show(playerPosition);
+				}
+			}
+		}
+	}
+}
