cf6b56e06b2e94a1ab84c11c607c683b8139e2b7
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-02 14:26:20 +0300

    Fix: set to each object only one text mesh

--- Assets/Scripts/Core/Animations/PopupScore.cs
+++ Assets/Scripts/Core/Animations/PopupScore.cs
@@ -10,21 +10,14 @@ namespace Core.Animations {
 
 		[Header("Configuration")]
 		[SerializeField] private Vector3 popupOffsetFromTarget = new Vector3(0, 1, 0);
-
 		[SerializeField] private Vector3 popupRotation = new Vector3(45, 0, 0);
 		[SerializeField] private float scaleDuration = 0.5f;
 
-		private const int CountOfPopups = 10;
-		private int currentPopUp = 0;
-		private Vector3 invisiblePosition = new Vector3(-999, -999, -999);
-		private List<GameObject> _popupScores;
+		private GameObject _popup;
 
 		private void Awake() {
-			_popupScores = new List<GameObject>(CountOfPopups);
-			for (int i = 0; i < CountOfPopups; i++) {
-				var popup = Instantiate(popupScorePrefab, invisiblePosition, default);
-				_popupScores.Add(popup);
-			}
+			_popup = Instantiate(popupScorePrefab);
+			SetNotActive();
 		}
 
 		public void Show(Vector3 position) {
@@ -32,17 +25,21 @@ namespace Core.Animations {
 		}
 
 		private void ShowPopupScore(Vector3 position) {
-			int index = currentPopUp % CountOfPopups;
-			Transform popup = _popupScores[index].transform;
+			_popup.SetActive(true);
+			Transform popup = _popup.transform;
 			Sequence _mySequence = DOTween.Sequence();
 			_mySequence
 				.Append(popup.DOMove(position + popupOffsetFromTarget, 0f))
 				.Append(popup.DOScale(Vector3.zero, 0f))
 				.Append(popup.DORotate(popupRotation, 0f))
 				.Append(popup.DOScale(Vector3.one, scaleDuration))
-				.Append(popup.DOMove(invisiblePosition, 0f));
+				.OnComplete(SetNotActive);
 
 			_mySequence.Play();
+		}
+
+		private void SetNotActive() {
+			_popup.SetActive(false);
 		}
 	}
 }
