142aca94d281d09ff8561f523d813d42978bf90a
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-04 21:36:31 +0300

    Add: Resource component
    gameobject container, which contain some information about Resource

---
+++ Assets/Scripts/Core/DataComponents/ResourceComponent.cs
@@ -0,0 +1,12 @@
+﻿using Data.Resource;
+using UnityEngine;
+
+namespace Core.DataComponents {
+	public class ResourceComponent : MonoBehaviour {
+		[SerializeField] private ResourceType resourceType;
+		[SerializeField] private int count;
+
+		public ResourceType Type => resourceType;
+		public int Count => count;
+	}
+}
