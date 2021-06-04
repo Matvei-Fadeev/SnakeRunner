84d5027730a009c0916d37f27dd11ae3bdfe21ae
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-04 21:31:22 +0300

    Add: realization of changing ParticleSystem StartedColor

---
+++ Assets/Scripts/Core/MaterialChanger/MaterialChangers/ColorChangerOfParticleSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Core.MaterialChanger.MaterialChangers {
+	public class ColorChangerOfParticleSystem : MonoBehaviour, IMaterialChanger {
+		public void SetMaterial(Material material) {
+			if (TryGetComponent(out ParticleSystem particleSystem)) {
+				ParticleSystem.MainModule particleSystemMain = particleSystem.main;
+				particleSystemMain.startColor = material.color;
+			}
+			else {
+				throw new UnityException($"Set the MeshRenderer to the {gameObject.name}");
+			}
+		}
+	}
+}
