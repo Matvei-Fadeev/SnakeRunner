f5311900cd1124a5721b3375c35934d26fbdc37b
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-04 21:25:45 +0300

    Add: ColorHolder - container for gameobjects

---
+++ Assets/Scripts/Data/Color/ColorHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Data.Color {
+	public class ColorHolder : MonoBehaviour {
+		[SerializeField] private GameColor[] gameColors;
+
+		public UnityEngine.Color GetColorByType(ColorType colorType) {
+			return GetMaterialByType(colorType).color;
+		}
+
+		public Material GetMaterialByType(ColorType colorType) {
+			foreach (var gameColor in gameColors) {
+				if (gameColor.colorType == colorType) {
+					return gameColor.material;
+				}
+			}
+
+			throw new Exception("Set the " + colorType);
+		}
+	}
+}
