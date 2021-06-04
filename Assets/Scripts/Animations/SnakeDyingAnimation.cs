a8ca7258fd34d77d1c29b216e927e1789e0112a2
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-04 21:28:33 +0300

    Add: snake dying animation

---
+++ Assets/Scripts/Animations/SnakeDyingAnimation.cs
@@ -0,0 +1,19 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Animations {
+	class SnakeDyingAnimation : MonoBehaviour, IAnimation {
+		[SerializeField] private float jumpHeight = 10f;
+		[SerializeField] private float jumpDuration = 4f;
+		[SerializeField] private Vector3 jumpOffset = new Vector3(10, -10, 0);
+
+		public void Show(Vector3 currentPosition) {
+			DyingAnimation(currentPosition);
+		}
+
+		private void DyingAnimation(Vector3 position) {
+			Vector3 underPlanePosition = position + jumpOffset ;
+			transform.DOJump(underPlanePosition, jumpHeight, 1, jumpDuration, false);
+		}
+	}
+}
