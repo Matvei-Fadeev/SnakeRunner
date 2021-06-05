36eaa0806252c158f3e51c110ebdaf1b76163e86
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-05 13:26:14 +0300

    Update: add animation to textChanger

--- Assets/Scripts/UI/Text/TextChanger.cs
+++ Assets/Scripts/UI/Text/TextChanger.cs
@@ -1,31 +1,51 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace UI.Text {
 	/// <summary>
 	/// Set on the Text GameObject
 	/// </summary>
 	public class TextChanger : MonoBehaviour {
-		private UnityEngine.UI.Text _text;
+		[Header("Configuration")]
+		[SerializeField] private float delayBetweenTextUpdates = 0.1f;
+		[SerializeField] private int maxCountPerUpdate = 1;
+
+		private TextMeshProUGUI _text;
 		private int _count;
+		private int _displayedCount;
+		private bool _alreadyIncrease;
 
 		private int Count {
 			get => _count;
 			set {
 				_count = value;
-				UpdateText();
+				if (!_alreadyIncrease) {
+					_alreadyIncrease = true;
+					InvokeRepeating(nameof(AnimatedIncreaseDisplayedCount), 0f, delayBetweenTextUpdates);
+				}			
 			}
 		}
 
 		private void Awake() {
-			_text = GetComponent<UnityEngine.UI.Text>();
+			_text = GetComponent<TextMeshProUGUI>();
 		}
-		
+
+		private void AnimatedIncreaseDisplayedCount() {
+			_displayedCount += maxCountPerUpdate;
+			if (_displayedCount > _count) {
+				_displayedCount = _count;
+				_alreadyIncrease = false;
+			}
+
+			UpdateText(_displayedCount);
+		}
+
 		protected void ReceiveValue(object sender, int newCount) {
 			Count = newCount;
 		}
 
-		private void UpdateText() {
-			_text.text = _count.ToString();
+		private void UpdateText(int count) {
+			_text.text = count.ToString();
 		}
 	}
 }
