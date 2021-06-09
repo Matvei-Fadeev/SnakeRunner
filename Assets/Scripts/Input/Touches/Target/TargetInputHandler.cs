4c9576c04238e64c458d423653bcbb55c22984b5
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-06-09 15:58:16 +0300

    Update: separate the touch and target input handle on abstract and realization class

---
+++ Assets/Scripts/Input/Touches/Target/TargetInputHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Input.Touches.Target {
+	public abstract class TargetInputHandler : InputHandler {
+		[Header("Target to calculate direction")]
+		[SerializeField] private string targetTag = "Player";
+
+		[Tooltip("Direction is normilized and independent from distance between target and touch position")]
+		[SerializeField] protected bool useNormalizedDirection = true;
+
+		protected Transform Target;
+		protected UnityEngine.Camera Camera;
+
+		protected new void Awake() {
+			base.Awake();
+			
+			Target = GameObject.FindGameObjectWithTag(targetTag).transform;
+			Camera = UnityEngine.Camera.main;
+		}
+
+		public override float GetVertical() {
+			return GetDirection().y;
+		}
+
+		public override float GetHorizontal() {
+			return GetDirection().x;
+		}
+	}
+}
